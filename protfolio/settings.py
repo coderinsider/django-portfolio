6407fc526b3e79199bf55c67d3a1af2faa679a36
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-22 16:22:46 +0630

    Change directory

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'protfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "portfoli_dbase",
+        "USER": "postgres",
+        "PASSWORD": "h3k",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
