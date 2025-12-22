9988263e15dc9ff3ab30b885978f29d93a1be0f4
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-22 15:11:10 +0630

    Creating the models in Django

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    # store the image
+    image = models.ImageField(upload_to='images/')
+    # images, 
+    # summary
+    summary  = models.CharField(max_length=200)
